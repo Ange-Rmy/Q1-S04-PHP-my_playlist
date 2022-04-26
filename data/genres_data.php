b92fb873ce0e84de58d9462ba92040d4aa936a5f
Ange-Rmy <93599535+Ange-Rmy@users.noreply.github.com>
2022-04-26 21:10:35 +0200

    Add files via upload
    Add genres_data.php

---
+++ data/genres_data.php
@@ -0,0 +1,73 @@
+<?php
+$genres = [
+    [
+        "id" => 1,
+        "name" => "House progressive",
+    ],
+    [
+        "id" => 2,
+        "name" => "Electro House",
+    ],
+    [
+        "id" => 3,
+        "name" => "Trance",
+    ],
+    [
+        "id" => 4,
+        "name" => "Hip-Hop",
+    ],
+    [
+        "id" => 5,
+        "name" => "Ambient",
+    ],
+    [
+        "id" => 6,
+        "name" => "Dubstep",
+    ],
+    [
+        "id" => 7,
+        "name" => "Musique électronique",
+    ],
+    [
+        "id" => 8,
+        "name" => "Electro Swing",
+    ],
+    [
+        "id" => 9,
+        "name" => "Neo Soul",
+    ],
+    [
+        "id" => 10,
+        "name" => "Nu-disco",
+    ],
+    [
+        "id" => 11,
+        "name" => "Synthpop",
+    ],
+    [
+        "id" => 12,
+        "name" => "House",
+    ],
+    [
+        "id" => 13,
+        "name" => "French touch",
+    ],
+    [
+        "id" => 14,
+        "name" => "Electro-rock",
+    ],
+    [
+        "id" => 15,
+        "name" => "Rap",
+    ],
+    [
+        "id" => 16,
+        "name" => "Mandopop",
+    ],
+    [
+        "id" => 17,
+        "name" => "House music",
+    ],
+]
+
+?>
